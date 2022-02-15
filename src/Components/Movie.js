748ae25e90475a742179ab3b0ae814731e1f2ec4
e97m <majdalawi97@yahoo.com>
2022-02-15 22:44:22 +0200

    problem at style and pics

---
+++ src/Components/Movie.js
@@ -0,0 +1,37 @@
+import { React, useState } from 'react'
+import { Card, CardGroup, Button } from 'react-bootstrap';
+import ModaleMovie from './ModaleMovie';
+
+function Movie({ card }) {
+
+    const [showModal, setShowModal] = useState(false);
+    const [movie, setMovie] = useState();
+
+    return (
+        <div key={card.id}>
+            <CardGroup style={{ display: "flex", justifyContent: "space-around" }}>
+                {
+                    <div key={card.id}>
+                        <Card key={card.id} >
+                            <Card.Img variant="top" src={card.poster_path} />
+                            <Card.Body>
+                                <Card.Title>{card.title}</Card.Title>
+                                <Card.Text>
+                                    {card.overview }
+                                </Card.Text>
+                                <Card.Text>
+                                    {card.caption ? card.caption : "No Caption Added"}
+                                </Card.Text>
+                                <Button variant="primary" onClick={() => { setMovie(card); setShowModal(true) }} >Show Movie</Button>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                }
+            </CardGroup>
+
+            {showModal && <ModaleMovie show={showModal} movie={movie} handleColse={() => { setShowModal(false) }} updateCaption={card.updateCaption} />}
+        </div>
+    )
+}
+
+export default Movie
